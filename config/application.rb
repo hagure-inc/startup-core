5ab2e9d42f05aa6218ea799319886610e582d34c
Tomoko <aerial1217@gmail.com>
2016-06-09 00:32:02 +0900

    change name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PjKodomoto
+module StartupCore
   class Application < Rails::Application
     config.generators do |g|
       g.template_engine :slim
