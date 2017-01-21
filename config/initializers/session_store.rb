9b843db4c5a130c9d2e89004ac299a5bf2fb880c
hagure-tomoko <ueoka_tomoko@hagure.info>
2017-01-21 23:09:58 +0900

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pj-kodomoto_session'
+Rails.application.config.session_store :cookie_store, key: '_startup_core_session'
